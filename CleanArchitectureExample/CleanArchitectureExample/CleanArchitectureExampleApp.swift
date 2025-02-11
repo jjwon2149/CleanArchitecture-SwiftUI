8d615cedcc77512b09b7cd8bedeb2385115236eb
jjwon2149 <jjwon2149@gmail.com>
2025-02-11 16:24:47 +0900

    Initialize CleanArchitectureExample Project

---
+++ CleanArchitectureExample/CleanArchitectureExample/CleanArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureExampleApp.swift
+//  CleanArchitectureExample
+//
+//  Created by 정종원 on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
