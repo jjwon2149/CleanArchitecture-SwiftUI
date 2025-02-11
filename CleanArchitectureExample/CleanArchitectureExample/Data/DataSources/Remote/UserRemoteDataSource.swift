db73807ffc8e3b6b6867aa2d7387d4aef5463701
jjwon2149 <jjwon2149@gmail.com>
2025-02-11 16:48:50 +0900

    feat: UserRemoteDataSource에서 API 호출

---
+++ CleanArchitectureExample/CleanArchitectureExample/Data/DataSources/Remote/UserRemoteDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  UserRemoteDataSource.swift
+//  CleanArchitectureExample
+//
+//  Created by 정종원 on 2/11/25.
+//
+
+import Foundation
+import Combine
+
+class UserRemoteDataSource {
+    func fetchUsers() -> AnyPublisher<[User], Error> {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {
+            return Fail(error: URLError(.badURL))
+                .eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [User].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
